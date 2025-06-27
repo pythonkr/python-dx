5e8c0cbd921579fc7f9e02eb84edd14d6170ae92
Bae KwonHan <darjeeling@gmail.com>
2025-06-27 14:21:30 +0900

    feat: Add GitHub Actions workflow for Sphinx documentation
    - Create a GitHub Actions workflow to automatically build Sphinx documentation and deploy it to GitHub Pages.
    - Use 'uv' for faster dependency management and builds.
    - Update pyproject.toml with project description and URLs.
    - Add .gitignore to exclude unnecessary files.

---
+++ source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Python DX for Python User Group of Korea'
+copyright = '2025, Python User Group Korea'
+author = 'Python User Group Korea'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinxcontrib.mermaid',
+    'sphinx_copybutton',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ko'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,19 @@
+.. python-dx documentation master file, created by
+   sphinx-quickstart on Fri Jun 27 13:50:23 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+python-dx documentation
+=======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   task-queue/index
+
